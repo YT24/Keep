f10cc659d3dba69efe9556a97ec183544489b7da
root <password>
2022-06-29 23:58:31 +0800

    sso+gateway+common

---
+++ sso/src/test/java/com/keep/sso/SsoApplicationTests.java
@@ -0,0 +1,14 @@
+package com.keep.sso;
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class SsoApplicationTests {
+
+    @Test
+    void test(){
+        System.out.println(1234567890);
+    }
+}
