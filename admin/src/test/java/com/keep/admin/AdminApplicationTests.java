b8cc046e68cfceb0f0a214fc29c541854bf54cdc
yangte <yangte@entropyreduce.com>
2022-06-30 16:35:23 +0800

    模块添加修改

---
+++ admin/src/test/java/com/keep/admin/AdminApplicationTests.java
@@ -0,0 +1,15 @@
+package com.keep.admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class AdminApplicationTests {
+
+
+    @Test
+    void test(){
+        System.out.println(2134567);
+    }
+
+}
---
+++ app/src/test/java/com/keep/app/AppApplicationTests.java
@@ -0,0 +1,7 @@
+package com.keep.app;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class AppApplicationTests {
+}
